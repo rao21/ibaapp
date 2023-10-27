e81d5293e72ee8f745e7b520aa07b1da6216b246
rao21 <rao.noman786@outlook.com>
2023-10-27 06:43:06 +0500

    Solution

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ibaapp/login/login.dart';
 import 'package:ibaapp/mid/mid.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyanAccent),
         useMaterial3: true,
       ),
-      home: const StockList(),
+      home: LoginScreen(),
     );
   }
 }
