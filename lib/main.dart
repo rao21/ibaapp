aad1c63bf959572a2f754f61a6212bcd04fd701d
rao21 <rao.noman786@outlook.com>
2023-11-16 20:56:08 +0500

    Firebase configure

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:ibaapp/firebase_options.dart';
 import 'package:ibaapp/login/login.dart';
 import 'package:ibaapp/mid/mid.dart';
 
-void main() {
+void main() async{
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
